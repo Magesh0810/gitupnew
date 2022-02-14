9fc4662f1ee993c00665ed980e3ff8e2ed7dff63
Magesh0810 <maadymagesh01@gmail.com>
2022-02-14 12:57:17 +0530

    ashikgit

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,18 @@
+package org.emp;
+
+import org.testng.annotations.Test;
+
+//class
+public class Employee {
+	
+	@Test
+	public void sampleTest() {
+		System.out.println("maddy");
+		
+		
+		
+	}
+ }
+	
+
+
